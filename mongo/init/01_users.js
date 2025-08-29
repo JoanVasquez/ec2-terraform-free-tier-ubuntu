00c0f4ca9266a1b77a66408d239f4af9200a0724
Joan Vasquez <dev.joanvasquez@gmail.com>
2025-08-29 14:55:55 -0400

    Update 01_users.js

--- mongo/init/01_users.js
+++ mongo/init/01_users.js
@@ -1,47 +1,18 @@
-(function () {
-  const dbname =
-    (typeof process !== "undefined" &&
-      process.env &&
-      process.env.MONGO_INITDB_DATABASE) ||
-    "uapaSoftwareLibreDB";
-  const users = [
-    {
-      u: "ubuntu",
-      p: "ubuntupass123",
-      roles: [{ role: "dbOwner", db: dbname }],
-    },
-    {
-      u: "david",
-      p: "davidpass123",
-      roles: [{ role: "readWrite", db: dbname }],
-    },
-    {
-      u: "frank",
-      p: "frankpass123",
-      roles: [{ role: "readWrite", db: dbname }],
-    },
-    {
-      u: "joanv",
-      p: "joanvpass123",
-      roles: [{ role: "readWrite", db: dbname }],
-    },
-    { u: "joel", p: "joelpass123", roles: [{ role: "readWrite", db: dbname }] },
-    {
-      u: "josec",
-      p: "josecpass123",
-      roles: [{ role: "readWrite", db: dbname }],
-    },
-    {
-      u: "sarai",
-      p: "saraipass123",
-      roles: [{ role: "readWrite", db: dbname }],
-    },
-  ];
 
-  const appdb = db.getSiblingDB(dbname);
-  users.forEach(({ u, p, roles }) => {
-    if (!appdb.getUser(u)) {
-      appdb.createUser({ user: u, pwd: p, roles });
-    }
+const dbName = 'uapaSoftwareLibreDB';
+const appDb = db.getSiblingDB(dbName);
+
+appDb.createUser({
+  user: "ubuntu",
+  pwd: "ubuntupass123",
+  roles: [ { role: "dbOwner", db: dbName } ]
+});
+
+// Usuarios con readWrite
+["david","frank","joanv","joel","josec","sarai"].forEach(u => {
+  appDb.createUser({
+    user: u,
+    pwd: `${u}pass123`,
+    roles: [ { role: "readWrite", db: dbName } ]
   });
-})();
+});
